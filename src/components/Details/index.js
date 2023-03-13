af2fd5d00123a5a44a99335f6042f5b7ca3c860e
James Stevens <jstevens.dev@gmail.com>
2023-03-13 17:01:55 -0400

    Changed BorderCountryLink url from common country name to official country name.

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -208,7 +208,7 @@ const Details = ({ country }) => {
                   return (
                     <BorderCountry key={i} darkMode={darkMode}>
                       <BorderCountryButton darkMode={darkMode}>
-                        <BorderCountryLink to={`../details/${border.name.common}`} darkMode={darkMode}>{border.name.common}</BorderCountryLink>
+                        <BorderCountryLink to={`../details/${border.name.official}`} darkMode={darkMode}>{border.name.common}</BorderCountryLink>
                       </BorderCountryButton>
                     </BorderCountry>
                   )
